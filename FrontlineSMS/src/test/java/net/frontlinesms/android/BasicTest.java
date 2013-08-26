bdab47eb71a7484e75cfd4168360c3d683ec1471
robhawkins <stillastudent@gmail.com>
2013-08-26 22:56:04 +0700

    attempting to setup first test, not working, robolectric error

--- FrontlineSMS/src/test/java/net/frontlinesms/android/BasicTest.java
+++ FrontlineSMS/src/test/java/net/frontlinesms/android/BasicTest.java
@@ -1,13 +1,41 @@
 package net.frontlinesms.android;
 
+import android.widget.Button;
+import android.widget.TextView;
+
+import net.frontlinesms.android.activity.KeywordList;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.tester.android.view.TestMenu;
+import org.robolectric.tester.android.view.TestMenuItem;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 
+
+@RunWith(RobolectricTestRunner.class)
 public class BasicTest {
-  @Test public void testBasic() {
-    Integer a = new Integer(7);
-    assertThat(a.equals(7));
-  }
+    private KeywordList keywordList;
+    private Button pressMeButton;
+    private TextView results;
+
+    @Before
+    public void setUp() throws Exception {
+        KeywordList keywordList = Robolectric.buildActivity(KeywordList.class).create().get();
+    }
+
+    @Test
+    public void testBasic() throws Exception {
+        TestMenu menu = new TestMenu();
+        keywordList.onPrepareOptionsMenu(menu);
+        TestMenuItem saveMenuItem = (TestMenuItem) menu.getItem(0);
+        assertThat(saveMenuItem.isEnabled(), equalTo(true));
+    }
 }
 
